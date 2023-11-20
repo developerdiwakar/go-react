ba2aa44e67ab5f4c863a170a4671cb8a1b8e2c3a
Diwakar <diwakar@cryptoforce.in>
2023-11-20 17:44:48 +0530

    Integrate Bootstrap Css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Goreact - CRUD App</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <div className="card-body">
+          <div className="card-title">
+            <p className="fw-bold pt-3">Counter Button</p>
+          </div>
+          <div className="d-grid">
+            <button
+              className="btn btn-primary btn-block"
+              onClick={() => setCount((count) => count + 1)}
+            >
+              count is <span className="fw-bold">{count}</span>
+            </button>
+          </div>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
