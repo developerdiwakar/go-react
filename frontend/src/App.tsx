f4603fe0a8f4d3782f70930ec5e82125a22d927a
Diwakar <diwakar@cryptoforce.in>
2023-11-20 18:21:06 +0530

    Segregate Counter card into a separate component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,10 @@
-import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <h1>Goreact - CRUD App</h1>
-      <div className="card">
-        <div className="card-body">
-          <div className="card-title">
-            <p className="fw-bold pt-3">Counter Button</p>
-          </div>
-          <div className="d-grid">
-            <button
-              className="btn btn-primary btn-block"
-              onClick={() => setCount((count) => count + 1)}
-            >
-              count is <span className="fw-bold">{count}</span>
-            </button>
-          </div>
-        </div>
-      </div>
     </>
   );
 }
