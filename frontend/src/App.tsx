f44c7329d76f91e2a626db5350ccaba1de86d76d
Diwakar <diwakar@cryptoforce.in>
2023-11-30 16:26:56 +0530

    Frontend - Added Dashboard html pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,21 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap-utilities.min.css";
 import "./App.css";
+import Navbar from "./components/main/Navbar";
+import Sidebar from "./components/main/Sidebar";
+import PageBody from "./components/main/PageBody";
 
 function App() {
   return (
-    <>
-      <h1>Goreact - CRUD App</h1>
-    </>
+    <div>
+      <Navbar />
+      <div className="container-fluid">
+        <div className="row">
+          <Sidebar />
+          <PageBody />
+        </div>
+      </div>
+    </div>
   );
 }
 
