895960f4fbdc21b8be7d18051a1fd1e02e5d0cd0
Diwakar <diwakar@cryptoforce.in>
2024-01-17 20:47:24 +0530

    Update the json fields for User model

--- backend/models/User.go
+++ backend/models/User.go
@@ -7,13 +7,13 @@ import (
 )
 
 type User struct {
-	ID           uint `gorm:"primaryKey"`
-	CreatedAt    time.Time
-	UpdatedAt    time.Time
+	ID           uint           `gorm:"primaryKey" json:"id"`
+	CreatedAt    time.Time      `json:"-"`
+	UpdatedAt    time.Time      `json:"-"`
 	DeletedAt    gorm.DeletedAt `gorm:"index" json:"-"`
-	Name         string         `gorm:"type:varchar(20); not null"`
-	MobileNumber string         `gorm:"uniqueIndex;size:15; not null"`
-	Email        string         `gorm:"uniqueIndex;size:30; not null"`
+	Name         string         `gorm:"type:varchar(20); not null" json:"name"`
+	MobileNumber string         `gorm:"uniqueIndex;size:15; not null" json:"mobile_number"`
+	Email        string         `gorm:"uniqueIndex;size:30; not null" json:"email"`
 	Password     string         `json:"-"`
-	Confirmed    *string        `gorm:"default:1"`
+	Confirmed    *string        `gorm:"default:1" json:"confirmed"`
 }
